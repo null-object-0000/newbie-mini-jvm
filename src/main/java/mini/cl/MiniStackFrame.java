0f472361b6483827cf23468983ad2e285413dd04
nce40202 <nce40202@ly.com>
2025-03-17 15:41:43 +0800

    refactor: 移除栈顶缓存变量，简化 MiniStackFrame 类

--- src/main/java/mini/cl/MiniStackFrame.java
+++ src/main/java/mini/cl/MiniStackFrame.java
@@ -23,8 +23,4 @@ public class MiniStackFrame {
      * 操作数栈
      */
     private final MiniStack<Integer> operandStack = new MiniStack<>();
-    /**
-     * 栈顶缓存变量（模拟寄存器）
-     */
-    private Integer tosCache;
 }
